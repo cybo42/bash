3e58541e0fd8e13dde0e92b6dc62df72ec974680
Joe Cyboski <cybo42@gmail.com>
2017-10-18 15:57:50 -0400

    added cdmkdir and highlight (hl) functions

--- functions-misc.sh
+++ functions-misc.sh
@@ -3,4 +3,12 @@ funfact(){
   curl -s  http://randomfunfacts.com |perl -n -e 'print "$1" if /<strong><i>(.*?)<\/i><\/strong>/m';
 }
 
+function cdmkdir(){
+  mkdir -p "$@" && cd $_;
+}
 
+
+hl () 
+{ 
+    perl -pe "s/$1/\e[0;30;42m$&\e[0m/g"
+}
