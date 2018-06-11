5a8228be57a326700ffaa2f1f8572deef6e6201f
Joe Cyboski <cybo42@gmail.com>
2018-06-11 14:28:04 -0400

    Adding alias file

---
+++ alias-misc.sh
@@ -0,0 +1,18 @@
+# SSH w/o checking hostname (for cloud hosts)
+alias ssh_nh='ssh -o StrictHostKeyChecking=no'
+
+alias ws='python -m SimpleHTTPServer'
+alias stree='open -a SourceTree'
+
+#-----------------------------------------
+# Common aliases
+#-----------------------------------------
+alias ls='ls -F'
+alias l='ls -l'
+alias ll='ls -la'
+alias c=clear
+
+# Stops control-C from closing mysql prompt
+alias mysql='mysql --sigint-ignore'
+
+
