62faa043cf269316ec6e6e32cfef0f063948da25
Joe Cyboski <cybo42@gmail.com>
2018-06-11 14:05:42 -0400

    updated dns flush

--- functions-osx.sh
+++ functions-osx.sh
@@ -2,18 +2,18 @@ function dnsflush(){
   #OS X Yosemite +
   #Use the following Terminal command to reset the DNS cache:
   #OS X 10.10+ (Yosemite):
-  sudo discoveryutil mdnsflushcache
+  #sudo discoveryutil mdnsflushcache
 
   #OS X Mavericks, Mountain Lion, and Lion
   # OS X 10.7–10.9 (Lion, Mountain Lion, Mavericks):
 
 
   #Use the following Terminal command to reset the DNS cache:
-  #sudo killall -HUP mDNSResponder
+  sudo killall -HUP mDNSResponder
 
   #Mac OS X v10.6
   #OS X pre-10.7 (before Lion):
   #Use the following Terminal command to reset the DNS cache:
-  #sudo dscacheutil -flushcache
+  sudo dscacheutil -flushcache
 }
 
