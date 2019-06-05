2363ab96c511301e13ed6b69bd0fa92db476966e
Joe Cyboski <cybo42@gmail.com>
2019-06-05 00:23:17 -0400

    adding android env

---
+++ env-android.sh
@@ -0,0 +1,11 @@
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+
+pathprepend $ANDROID_HOME/tools
+pathprepend $ANDROID_HOME/platform-tools
+
+pathprepend $ANDROID_HOME/tools/bin
+pathprepend $ANDROID_HOME/emulator
+
+
+
