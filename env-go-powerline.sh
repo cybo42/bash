8ed208c5ca7cba65ce6f4eb63be21371aecec13d
Mobile Automation <app-platforms@nytimes.com>
2024-05-21 16:26:44 -0400

    powerline

--- env-go-powerline.sh
+++ env-go-powerline.sh
@@ -1,6 +1,11 @@
 
 function _update_ps1() {
-    PS1="$(~/apps/powerline-go/powerline-go -cwd-max-depth 3 -newline -numeric-exit-codes -modules "venv,user,host,ssh,cwd,perms,git,hg,jobs,exit,root,node" -error $?)"
+    PS1="$(~/apps/powerline-go/powerline-go \
+      -cwd-max-depth 3 \
+      -newline \
+      -numeric-exit-codes \
+      -modules "venv,user,host,ssh,cwd,perms,git,hg,jobs,exit,root,node,aws" -error $?
+    )"
 }
 
 if [ "$TERM" != "linux" ]; then
