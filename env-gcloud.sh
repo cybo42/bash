b57783210eb10874c09f52075db77fb7d0c81d21
Mobile Automation <app-platforms@nytimes.com>
2024-05-21 16:26:35 -0400

    fix glcoud

--- env-gcloud.sh
+++ env-gcloud.sh
@@ -1,10 +1,6 @@
+
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f $HOME/apps/google-cloud-sdk/path.bash.inc ]; then
-  source "$HOME/apps/google-cloud-sdk/path.bash.inc"
-fi
+if [ -f '/Users/jcyboski/apps/google-cloud-sdk/path.bash.inc' ]; then . '/Users/jcyboski/apps/google-cloud-sdk/path.bash.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f $HOME/apps/google-cloud-sdk/completion.bash.inc ]; then
-  source "$HOME/apps/google-cloud-sdk/completion.bash.inc"
-fi
-
+if [ -f '/Users/jcyboski/apps/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/jcyboski/apps/google-cloud-sdk/completion.bash.inc'; fi
