d27b45ec031bba28bb884d9884be63b75e14eb3c
Mobile Automation <app-platforms@nytimes.com>
2024-05-21 16:24:44 -0400

    update android tools

--- env-android.sh
+++ env-android.sh
@@ -1,11 +1,12 @@
 
 export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
 
-pathprepend $ANDROID_HOME/tools
+# Command line tools
+pathprepend $ANDROID_HOME/cmdline-tools/latest/bin
+# AOSP tools
 pathprepend $ANDROID_HOME/platform-tools
-
-pathprepend $ANDROID_HOME/tools/bin
+# Emulator
 pathprepend $ANDROID_HOME/emulator
-
 
 
