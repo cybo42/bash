0bb65fc49e2f36879bbea78c3fb39830cb26c72b
Joe <cybo42@gmail.com>
2025-09-16 09:52:49 -0400

    Adding docker-ctx context switcher function

--- env-docker.sh
+++ env-docker.sh
@@ -1,3 +1,13 @@
 export DOCKER_HOME=/Applications/Docker.app/Contents/Resources/bin
 
 export PATH=$DOCKER_HOME:$PATH
+
+function docker-ctx () {
+  local context_list
+  context_list=$(docker context list)
+
+  echo ""
+  echo "---------------------------------------------------\n"
+  docker context use $(docker context list -q | fzf --footer "$context_list"  --highlight-line)
+
+}
